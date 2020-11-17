e4101a1e00a2a35327f72f0fac9cf8e0e34aed3d
elijahsamuels <elijahsamuels@gmail.com>
2020-11-16 20:13:54 -0700

    commit tree 6b782144705b6580a52746c11597fd505bae03ba

--- lib/meowing_cat.rb
+++ lib/meowing_cat.rb
@@ -1,1 +1,17 @@
+# 1. Define a class, called Cat.
+# 2. Use the `attr_accessor` macro to create a setter and getter method for a cat's name.
+# 4. Write a method, `.meow`, that outputs "meow!" to the terminal using the `puts` method when called on an instance of Cat.
+
+
+
 ## code your solution here. 
+
+class Cat
+
+    attr_accessor :name
+
+    def meow
+        puts "meow!"
+    end
+end
+
